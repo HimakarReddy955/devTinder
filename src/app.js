f615d9555a253328842759111e09a7adbec002ff
Himakar Reddy <himakar.reddy@informa.com>
2025-03-11 17:24:53 +0530

    (feat): user schema

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
-const express = require('express');
+const express = require("express");
+
+const connectDB = require("./config/database");
+
+const User = require("./models/user");
 
 const app = express();
 
-app.use("/hello", (req,res) => {
-    res.send("Hello from the server!");
-})
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Deepender",
+    lastName: "sigh",
+    emailId: "Deep@email.com",
+    password: "Deepender",
+  };
 
-app.use("/Himakar", (req,res) => {
-    res.send("Himakar from the server!");
-})
+  const user = new User(userObj);
 
-app.listen(3333, ()=> {
-    console.log("Server is successfully running on port 3333....");
+  try {
+    await user.save();
+    res.send("User Added Successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
 });
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3333, () => {
+      console.log("Server is successfully running on port 3333....");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://himakarreddy955:Himakar@955@namastenode.cojhn.mongodb.net/"
-    )
-}
-
+  await mongoose.connect(
+    "mongodb+srv://himakarreddy955:Himakar955@namastenode.cojhn.mongodb.net/devTinder"
+  );
+};
 
 connectDB()
-    .then(() => {
-        console.log("Database connection established...")
-    })
-    .catch((err) => {
-        console.error("Database connection failed...")
-    })
+  .then(() => {
+    console.log("Database connection established...");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
+module.exports = connectDB;
